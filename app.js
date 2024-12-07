36c83a007862e47dfca49a2d5799c8a51a9aa6a3
Davy IYANGUI <dave.iyangui@gmail.com>
2024-12-07 17:37:57 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome to K21Academy!'));
+app.get('/', (req, res) => res.send('We have added a review stage with manual approval to our continuous delivery pipeline!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
